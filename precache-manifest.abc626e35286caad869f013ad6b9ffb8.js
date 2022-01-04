90cb68f51505bf551af4980822c096c7349de15c
Code Sandbox <hello@codesandbox.io>
2022-01-04 02:06:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.abc626e35286caad869f013ad6b9ffb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fc08ca6de4378e59387",
+    "url": "https://yusefallam.github.io/csb-5i8dv/static/css/main.9855d1e1.chunk.css"
+  },
+  {
+    "revision": "1fc08ca6de4378e59387",
+    "url": "https://yusefallam.github.io/csb-5i8dv/static/js/main.9a026eac.chunk.js"
+  },
+  {
+    "revision": "94d3bc2b99b6d6109d05",
+    "url": "https://yusefallam.github.io/csb-5i8dv/static/js/runtime~main.fb581d71.js"
+  },
+  {
+    "revision": "413cea279d8a208b4992",
+    "url": "https://yusefallam.github.io/csb-5i8dv/static/css/2.4193a437.chunk.css"
+  },
+  {
+    "revision": "413cea279d8a208b4992",
+    "url": "https://yusefallam.github.io/csb-5i8dv/static/js/2.1448f1a3.chunk.js"
+  },
+  {
+    "revision": "d055c43fa46e4e45f229d252a3c29b33",
+    "url": "https://yusefallam.github.io/csb-5i8dv/index.html"
+  }
+];
